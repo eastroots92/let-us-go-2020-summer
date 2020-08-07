1f449d665d603848faf70e169f3484716c207b4a
Donggeun Lee <eastroots92@gmail.com>
2020-08-07 18:16:25 +0900

    Add icon

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -1,5 +1,8 @@
 import S from './styles';
 import React from "react";
+import badgeIcon1 from '../../resources/badge.svg';
+import badgeIcon2 from '../../resources/badge2.svg';
+import badgeIcon3 from '../../resources/mask.png';
 
 export const Hero = () => {
   return (
@@ -12,6 +15,9 @@ export const Hero = () => {
       <S.Contents>
         2020년 8월 15일 13:00~18:00
       </S.Contents>
+      <S.Badge1 src={badgeIcon1} />
+      <S.Badge2 src={badgeIcon2} />
+      <S.Badge3 src={badgeIcon3} />
     </S.Header>
   )
 }
--- src/components/hero/styles.js
+++ src/components/hero/styles.js
@@ -1,4 +1,67 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components"
+
+const aniSpin = keyframes`
+  0% {
+    transform: rotate(0) translateZ(0);
+  }
+  100% {
+    transform: rotate(360deg) translateZ(0);
+  }
+`;
+
+const aniMoveX = keyframes`
+  0% {
+    left: 0;
+  }
+  100% {
+    left: calc(100% - 121px);
+  }
+`;
+
+const aniMoveY = keyframes`
+  0% {
+    top: 0;
+  }
+  100% {
+    top: calc(100vh - 121px);
+  }
+`;
+
+const aniMoveX2 = keyframes`
+  0% {
+    right: 0;
+  }
+  100% {
+    right: calc(100% - 98px);
+  }
+`;
+
+const aniMoveY2 = keyframes`
+  0% {
+    bottom: 0;
+  }
+  100% {
+    bottom: calc(100vh - 98px);
+  }
+`;
+
+const aniMoveX3 = keyframes`
+  0% {
+    right: 0;
+  }
+  100% {
+    right: calc(100% - 108px);
+  }
+`;
+
+const aniMoveY3 = keyframes`
+  0% {
+    top: 0;
+  }
+  100% {
+    top: calc(100vh - 108px);
+  }
+`;
 
 export default {
   Header: styled.header`
@@ -11,6 +74,7 @@ export default {
     align-items: center;
     padding: 0 1rem;
     position: relative;
+    overflow: hidden;
     
     @media only screen and (min-width: 450px) {
       align-items: center;
@@ -35,6 +99,8 @@ export default {
     word-break: keep-all;
     color: #f5f5f5;
     margin-bottom: 0.25rem;
+    text-align: center;
+    z-index: 5;
     
     @media only screen and (min-width: 450px) {
       margin: 2px 0 6px;
@@ -47,11 +113,62 @@ export default {
     letter-spacing: 3px;
     color: #4affcf;
     margin-bottom: 2rem;
+    z-index: 5;
   `,
   Contents: styled.p`
     font-size: 1rem;
     font-weight: normal;
     line-height: 1.25;
+    z-index: 5;
     color: #f5f5f5;
-  `
+  `,
+  Badge1: styled.img`
+    width: 115px;
+    height: 121px;
+    display: block;
+    position: absolute;
+    will-change: top, left;
+    top: 0;
+    left: 0;
+    z-index: 0;
+    transform: translateZ(0);
+    animation: ${aniMoveX} 3.5s linear 0s infinite alternate, ${aniMoveY} 4s linear 0s infinite alternate,  ${aniSpin} 6s linear 0s infinite;
+     
+    @media only screen and (min-width: 768px) {
+      animation: ${aniMoveX} 7s linear 0s infinite alternate, ${aniMoveY} 8s linear 0s infinite alternate,  ${aniSpin} 9.5s linear 0s infinite;
+    }
+  `,
+  Badge2: styled.img`
+    width: 98px;
+    height: 94px;
+    display: block;
+    position: absolute;
+    will-change: bottom, right;
+    bottom: 0;
+    right: 100%;
+    z-index: 10;
+    transform: translateZ(0);
+    animation: ${aniMoveX2} 4.5s linear 0s infinite alternate, ${aniMoveY2} 5.5s linear 0s infinite alternate, ${aniSpin} 6s linear 0s infinite;
+     
+    @media only screen and (min-width: 768px) {
+      animation: ${aniMoveX2} 9s linear 0s infinite alternate, ${aniMoveY2} 10.5s linear 0s infinite alternate, ${aniSpin} 8s linear 0s infinite;
+    }
+  `,
+  Badge3: styled.img`
+    width: 108px;
+    height: 80px;
+    display: block;
+    position: absolute;
+    will-change: top, right;
+    top: 0;
+    right: 10%;
+    z-index: 0;
+    transform: translateZ(0);
+    animation: ${aniMoveX3} 5s linear 0s infinite alternate, ${aniMoveY3} 4s linear 0s infinite alternate, ${aniSpin} 8s linear 0s infinite;
+    
+    @media only screen and (min-width: 768px) {
+      animation: ${aniMoveX3} 10s linear 0s infinite alternate, ${aniMoveY3} 8s linear 0s infinite alternate, ${aniSpin} 11s linear 0s infinite;
+    }
+  `,
 };
+
