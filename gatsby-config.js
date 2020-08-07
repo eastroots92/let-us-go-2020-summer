cf1aa66bed5da61bf895ed75c4d9bddc81870053
Donggeun Lee <eastroots92@gmail.com>
2020-08-07 18:51:05 +0900

    Fix animation trigger

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
       resolve: `gatsby-plugin-scroll-reveal`,
       options: {
         threshold: 0.5, // Percentage of an element's area that needs to be visible to launch animation
-        once: true, // Defines if animation needs to be launched once
+        once: false, // Defines if animation needs to be launched once
         disable: false, // Flag for disabling animations
 
         // Advanced Options
