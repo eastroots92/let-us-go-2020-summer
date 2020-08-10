12531bd463f8c2a6ece4025b9b3acb8fc7a20d23
Donggeun Lee <eastroots92@gmail.com>
2020-08-10 14:45:36 +0900

    Fix link

--- src/components/floating-button/index.js
+++ src/components/floating-button/index.js
@@ -3,11 +3,11 @@ import React from "react"
 import scheduleIcon from '../../resources/schedule.svg';
 import { GA_ACTION, GAClickEvent } from "../../utils/ga"
 
-const link = 'https://letusgo.s3.ap-northeast-2.amazonaws.com/letUsGo2020.ics';
+const downlaodLink = 'https://letusgo.s3.ap-northeast-2.amazonaws.com/letUsGo2020.ics';
 
 export const FloatingButton = () => (
   <S.Button
-    href={link}
+    href={downlaodLink}
     target='_blank'
     onClick={ GAClickEvent('플로팅_버튼', GA_ACTION.CLICK, '스케쥴') }
   >
