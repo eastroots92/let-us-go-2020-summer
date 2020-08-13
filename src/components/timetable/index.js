10e210c224d548fd1875f12b503dc56a14464a77
Jercy <wotjdzz1@naver.com>
2020-08-14 02:45:35 +0900

    Edit timeline

--- src/components/timetable/index.js
+++ src/components/timetable/index.js
@@ -14,32 +14,57 @@ const timetableList = [
     badge: ['야곰'],
   },
   {
+    label: '13:40',
+    title: '질의응답',
+    badge: ['야곰', 'Kuu', '재르시'],
+  },
+  {
     label: '13:50',
     title: '패널토크 : 외국의 개발자 생활은 어떻습니까?',
     badge: ['unnnyong', '노수진', '도길참새'],
   },
   {
     label: '14:50',
+    title: '질의응답',
+    badge: ['야곰', 'Kuu', '재르시'],
+  },
+  {
+    label: '15:00',
     title: '역전의 명수 (Inversions)',
     badge: ['곰튀김'],
   },
   {
     label: '15:20',
+    title: '질의응답',
+    badge: ['야곰', 'Kuu', '재르시'],
+  },
+  {
+    label: '15:30',
     title: 'Cocoa pod, Carthage, SPM 중 무엇을 쓰시나요?',
     badge: ['이현호'],
   },
   {
     label: '15:50',
+    title: '질의응답',
+    badge: ['야곰', 'Kuu', '재르시'],
+  },
+  {
+    label: '16:00',
     title: 'Apple Pay 개발을 못해도 어떻게 하는지 아는 건 죄가 아니잖아',
     badge: ['unnnyoung'],
   },
   {
     label: '16:20',
+    title: '질의응답',
+    badge: ['야곰', 'Kuu', '재르시'],
+  },
+  {
+    label: '16:30',
     title: '패널토크 : 신입 개발자, 이런 사람 찾아요.',
     badge: ['Genetory', '엉덩숭아', 'AnyObject', '클린트'],
   },
   {
-    label: '17:20',
+    label: '17:50',
     title: '폐회사',
     badge: [],
   },
